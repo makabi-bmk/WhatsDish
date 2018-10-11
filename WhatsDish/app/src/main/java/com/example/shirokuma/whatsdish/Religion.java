f2582486a99f3e8f310ade78ca7922f8fb1a972c
rio-k827 <mi161317@edu.okinawa-ct.ac.jp>
2018-10-11 15:24:38 +0900

    add image and religion setup

---
+++ WhatsDish/app/src/main/java/com/example/shirokuma/whatsdish/Religion.java
@@ -0,0 +1,9 @@
+package com.example.shirokuma.whatsdish;
+
+import android.support.v7.app.AppCompatActivity;
+
+import java.util.HashMap;
+
+public class Religion extends AppCompatActivity {
+    public HashMap<String, String> religion = new HashMap<>();
+}
