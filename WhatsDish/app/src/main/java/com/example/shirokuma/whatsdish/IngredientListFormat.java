72134efa9271bc474b2abb090cc299ae01a64d51
b-mk <youremailexample.com(mi161332@edu.okinawa-ct.ac.jp)>
2018-11-02 16:48:36 +0900

    食べられないものリストのadapterにnullが入ってる

--- WhatsDish/app/src/main/java/com/example/shirokuma/whatsdish/IngredientListFormat.java
+++ WhatsDish/app/src/main/java/com/example/shirokuma/whatsdish/IngredientListFormat.java
@@ -1,23 +1,36 @@
 package com.example.shirokuma.whatsdish;
 
 public class IngredientListFormat {
-    public int listID;
+    //public int listID;
     public String ingredientName;
     public boolean isPossibleToEat;
     Category category;
 
     IngredientListFormat(int listID, String ingredientName, Category category, boolean isPossibleToEat) {
-        this.listID = listID;
+        //this.listID = listID;
         this.ingredientName = ingredientName;
         this.isPossibleToEat = isPossibleToEat;
         this.category = category;
     }
 
     IngredientListFormat(int listID, String ingredientName, Category category) {
-        this.listID = listID;
+        //this.listID = listID;
         this.ingredientName = ingredientName;
         this.isPossibleToEat = true;
         this.category = category;
+    }
+
+    public int getListID() {
+        //return listID;
+        return 0;
+    }
+
+    public String getName() {
+        return ingredientName;
+    }
+
+    public boolean isPossibleToEat() {
+        return isPossibleToEat;
     }
 
     public enum Category {
--- WhatsDish/app/src/main/java/com/example/shirokuma/whatsdish/ShowIngredientList2.java
+++ WhatsDish/app/src/main/java/com/example/shirokuma/whatsdish/ShowIngredientList2.java
@@ -10,6 +10,8 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.util.ArrayList;
+
 import static java.sql.Types.NULL;
 
 public class ShowIngredientList2 extends AppCompatActivity {
@@ -17,16 +19,19 @@ public class ShowIngredientList2 extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.show_ingredientlist1);
+        setContentView(R.layout.show_ingredientlist2);
 
         int categoryNum = getIntent().getIntExtra("categoryNum", -1);
         //String categoryName = getResources().getString(getResources().getIdentifier("category_" + categoryNum, "string", getPackageName()));
         String[] categoryName = {"vegetables", "fruits", "meats", "seafoods", "seasonings", "grains", "dairy_products"};
+        IngredientListFormat.Category[] category = {IngredientListFormat.Category.vegetable, IngredientListFormat.Category.fruit, IngredientListFormat.Category.meat, IngredientListFormat.Category.seafood, IngredientListFormat.Category.seasoning, IngredientListFormat.Category.seasoning, IngredientListFormat.Category.grain, IngredientListFormat.Category.dairy_product};
+        ArrayList<IngredientListFormat> list = new ArrayList<IngredientListFormat>();
 
         Log.d("weiwei", "categoryNum = " + categoryNum);
         Log.d("weiwei", "categoryName = " + categoryName[categoryNum]);
         final ListView listView = findViewById(R.id.ingredient_list);
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_multiple_choice);
+        //ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_multiple_choice);
+        IngredientAdapter adapter = new IngredientAdapter(ShowIngredientList2.this);
         int ingredientNum;
 
         ingredientNum = 0;
@@ -35,10 +40,11 @@ public class ShowIngredientList2 extends AppCompatActivity {
             if (strID == NULL || strID == 0) {
                 break;
             }
-            adapter.add(getResources().getString(strID));
+            IngredientListFormat ingredientListFormat = new IngredientListFormat(0, getResources().getString(strID), category[categoryNum]);
+            list.add(ingredientListFormat);
             ingredientNum++;
         }
-
+        adapter.setIngredientList(list);
         listView.setAdapter(adapter);
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
